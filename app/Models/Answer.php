949bef5b1a650d57c875b7cc45dd11d153bb74e9
Shohrux <shohruxnizomov170@gmail.com>
2025-12-10 21:31:56 +0500

    javoblar un model yozildi

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,9 +2,46 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'prompt_id',
+        'answer',
+        'is_primary',
+        'helpfulness_score',
+    ];
+
+    protected $casts = [
+        'is_primary' => 'boolean',
+        'helpfulness_score' => 'integer',
+    ];
+
+    /**
+     * Get the prompt this answer belongs to
+     */
+    public function prompt()
+    {
+        return $this->belongsTo(Prompt::class);
+    }
+
+    /**
+     * Vote helpful
+     */
+    public function voteHelpful()
+    {
+        $this->increment('helpfulness_score');
+    }
+
+    /**
+     * Vote unhelpful
+     */
+    public function voteUnhelpful()
+    {
+        $this->decrement('helpfulness_score');
+    }
 }
