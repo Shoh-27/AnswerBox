408b65222575b98faa5edfaa53b8d3abbebaa2db
Shohrux <shohruxnizomov170@gmail.com>
2025-12-10 21:33:44 +0500

    yangi javob qoshish un create metod yozildi

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prompt;
+use App\Models\Answer;
 use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+    /**
+     * Show form to create new Q&A pair
+     */
+    public function create(Request $request)
+    {
+        $inputPrompt = $request->input('prompt', '');
+
+        return view('answers.create', compact('inputPrompt'));
+    }
+
+
 }
