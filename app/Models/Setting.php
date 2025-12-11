c98fe68b110cda65669b3ecd1903d9b593121721
Shohrux <shohruxnizomov170@gmail.com>
2025-12-11 22:06:05 +0500

    feat(settings): settings jadvali baza bn ishlashga moslashtriish un  model yozildi

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,50 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'key',
+        'value',
+        'type',
+        'description',
+    ];
+
+    /**
+     * Get typed value
+     */
+    public function getTypedValue()
+    {
+        return match($this->type) {
+            'integer' => (int) $this->value,
+            'float' => (float) $this->value,
+            'boolean' => filter_var($this->value, FILTER_VALIDATE_BOOLEAN),
+            default => $this->value,
+        };
+    }
+
+    /**
+     * Get setting by key
+     */
+    public static function getValue($key, $default = null)
+    {
+        $setting = self::where('key', $key)->first();
+        return $setting ? $setting->getTypedValue() : $default;
+    }
+
+    /**
+     * Set setting by key
+     */
+    public static function setValue($key, $value)
+    {
+        return self::updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
 }
